19bada573ce45c15ca8631aecd1da34d97268278
Andresmobl <157494359+Andresmobl@users.noreply.github.com>
2024-02-20 18:28:15 +0100

    #7

--- Ahorcado/src/ahorcado/Servidor.java
+++ Ahorcado/src/ahorcado/Servidor.java
@@ -8,6 +8,7 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Random;
 
 /**
  *
@@ -21,6 +22,12 @@ public class Servidor extends Thread { // extend Thread permite trabajar al serv
     // Declaracion de variables 
     Socket skCliente;
     static final int Puerto = 2000;
+    // Declaramos las palabras en funcion del nivel seleccionado
+    String[] palabrasFaciles = {"TOMATE","AMIGO","PERRO","GATO","RANA","PASTA","VASO","BOLI","TELE","PATATA"};
+    String[] palabrasMedias = {"CABEZAZO","JUGADORES","PROGRAMACION","ALTAVOCES","BICICLETA","COLGADOR","CARGADOR","CARRITO","MECHEROS","EXAMENES"};
+    String[] palabrasDificiles = {"AYUNTAMIENTO","SUBCONTRATAR","ESCAULLIRSE","ESTABLECERSE","ZIGZAGUEABAIS","DESINCORPORAR","JAZZ","ELECTROENCEFALOGRAMA","OTORRINOLARINGOLOGO","FOTOSINTESIS"};
+    String palabraElegida, palabraOculta, letra;
+    int intentos = 10;
     
     // Constructor que recibe el socket del cliente al que se va a atender
     public Servidor(Socket sCliente) {
@@ -58,9 +65,47 @@ public class Servidor extends Thread { // extend Thread permite trabajar al serv
             DataOutputStream flujo_salida = new DataOutputStream(skCliente.getOutputStream());
             
             // Solicitar al cliente que introduzca el nivel de dificultad
-            flujo_salida.writeUTF("Selecciona la dificultad(facil, medio, dificil) ");
+            flujo_salida.writeUTF("Selecciona la dificultad(facil, medio, dificil): ");
             String nivel = flujo_entrada.readUTF();
             System.out.println("\tEl cliente ha dicho " + nivel);
+            
+            // Seleccionamos la palabra aleatoria en funcion de la dificultad elegida por el cliente 
+            switch (nivel){
+                
+                case "facil": 
+                    palabraElegida = crearPalabraAleatoria(palabrasFaciles);
+                    palabraOculta = crearPalabraOculta(palabraElegida);
+                    
+                    flujo_salida.writeUTF(palabraOculta);
+                    flujo_salida.writeUTF("Quedan " + intentos + " intentos. Introduce una letra: ");
+                    letra = flujo_entrada.readUTF();
+                    System.out.println("\tEl cliente ha dicho " + letra);
+                    
+                    break;
+                            
+                case "medio":
+                    palabraElegida = crearPalabraAleatoria(palabrasMedias);
+                    palabraOculta = crearPalabraOculta(palabraElegida);
+                    
+                    flujo_salida.writeUTF(palabraOculta);
+                    flujo_salida.writeUTF("Quedan " + intentos + " intentos. Introduce una letra: ");
+                    letra = flujo_entrada.readUTF();
+                    System.out.println("\tEl cliente ha dicho " + letra);
+                    
+                    break;
+                            
+                case "dificil":
+                    palabraElegida = crearPalabraAleatoria(palabrasDificiles);
+                    palabraOculta = crearPalabraOculta(palabraElegida);
+                    
+                    flujo_salida.writeUTF(palabraOculta);
+                    flujo_salida.writeUTF("Quedan " + intentos + " intentos. Introduce una letra: ");
+                    letra = flujo_entrada.readUTF();
+                    System.out.println("\tEl cliente ha dicho " + letra);
+                    
+                    break;
+                
+            }        
             
         }catch (Exception e) {
             
@@ -70,6 +115,44 @@ public class Servidor extends Thread { // extend Thread permite trabajar al serv
         }
             
         
+    }
+    
+    public static String crearPalabraAleatoria(String[] nivelElegido){
+        
+        Random r = new Random();
+        int numeroAleatorio = r.nextInt(11);
+        
+        String palabraAleatoria = nivelElegido[numeroAleatorio];
+        
+        return palabraAleatoria;
+        
+    }
+    
+    public static String crearPalabraOculta(String palabraAleatoria){
+        
+        int longitudPalabra = palabraAleatoria.length();
+        String[] palabraOculta = new String[longitudPalabra];
+        String palabraFinal;
+        
+        for(int i=0; i<palabraOculta.length; i++){
+            palabraOculta[i] = "_";
+        }
+        
+        palabraFinal = (convertirArrayAString(palabraOculta));
+        
+        return palabraFinal;
+        
+    }
+    
+    // Función para convertir un array de strings en una sola palabra
+    public static String convertirArrayAString(String[] array) {
+        StringBuilder palabra = new StringBuilder();
+        
+        for (String letra : array) {
+            palabra.append(letra);
+        }
+        
+        return palabra.toString();
     }
     
 }
